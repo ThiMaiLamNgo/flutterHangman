0c650a7e5e019df8c3c529305fd9b87a366f5e9e
ThiMaiLamNgo <thimailamngo@gmail.com>
2021-12-05 20:24:22 +0000

    rammi

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,10 +1,68 @@
+import 'package:flutter/material.dart';
 import 'dart:io';
 import 'dart:math';
 import 'wordsBank.dart';
 
-void main() {
-  randomWord();
+void main() => runApp(HangMan());
+
+class HangMan extends StatelessWidget {
+  const HangMan({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: HangManPage(),
+          ),
+        ),
+      ),
+    );
+  }
 }
+
+class HangManPage extends StatefulWidget {
+  const HangManPage({Key? key}) : super(key: key);
+
+  @override
+  _HangManPageState createState() => _HangManPageState();
+}
+
+class _HangManPageState extends State<HangManPage> {
+  //todo st here
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                //todo the rules of the game
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(child: child)
+      ],
+    );
+  }
+}
+
+//class HangManPage extends
 
 List<String> wordBank = [
   'hello',
